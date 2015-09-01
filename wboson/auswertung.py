bf5f2d75271d128a59d49b62bbb59f4fd8ce9b90
Jonas Lieb <lieb.jonas@gmail.com>
2015-09-01 16:06:31 +0200

    Included uncertainty shifts

--- wboson/auswertung.py
+++ wboson/auswertung.py
@@ -3,11 +3,14 @@ import os, os.path
 import sys
 import contextlib
 import math
+import random
+#random.seed(33)
 
 import ROOT
 import numpy as np
 import matplotlib.pyplot as plt
-from uncertainties import ufloat
+from uncertainties import ufloat, umath, correlated_values_norm
+from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
 
 try:
 	from scipy.optimize import curve_fit
@@ -17,7 +20,7 @@ except ImportError:
 
 from rootwrapper import *
 from jhist import Hist
-#import jmpl
+import jmpl
 
 def weight_to_mass(i): 
     return [
@@ -41,6 +44,68 @@ def weight_to_mass(i):
         80.7946,
         80.8446
     ][i]
+
+def annotate(text, loc=1, frameon=True, **kwargs):
+    at = AnchoredText(text, loc=loc, frameon=frameon, prop=kwargs)
+    plt.gca().add_artist(at)
+    return at
+
+class MyEvent(object):
+    def __init__(self, event, smear_energy=False, scale_energy=None):
+        self._event = event
+
+        pT = math.sqrt(self._event.el_px**2 + self._event.el_py**2)
+        p = math.sqrt(self._event.el_px**2 + self._event.el_py**2 + self._event.el_pz**2)
+        E = self.el_e
+        self.el_e_t = pT/p * E
+
+        self.met_e_t = math.sqrt(self.metx_calo**2 + self.mety_calo**2)
+
+        if smear_energy:
+            self.el_e_t = self.dice_energy_resolution(self.el_e_t)
+
+        if scale_energy == "up":
+            self.el_e_t += self.energy_scale(self.el_e_t)
+        elif scale_energy == "down":
+            self.el_e_t -= self.energy_scale(self.el_e_t)
+        elif scale_energy is not None:
+            raise ValueError("Unknown scale direction '%s'." % scale_energy)
+
+        self.met_phi = math.atan2(self.metx_calo, self.mety_calo) + math.pi
+        self.delta_z = self.el_track_z - self.met_vertex_z
+        self.m_t = math.sqrt(self.el_e_t*self.met_e_t*(1-math.cos(self.el_met_calo_dphi)))
+
+    def __getattr__(self, name):
+        return getattr(self._event, name)
+        
+    def is_mc(self):
+        return hasattr(self._event, "weight")
+
+    def is_data(self):
+        return not self.is_mc()
+
+    def get_weight(self, i):
+        if self.is_mc():
+            return self._event.weight[i]
+        else:
+            return 1.0
+
+    def passes_cuts(self):
+        return (self.met_e_t > 20
+            and self.el_e_t > 30
+            and self.el_iso < 0.03 
+            and self.el_met_calo_dphi > 2.85
+            and abs(self.delta_z) < 0.2)
+
+    @staticmethod
+    def dice_energy_resolution(E):
+        sigma = 0.015 * E + 0.13*math.sqrt(E) + 0.4
+        return max(0, random.gauss(E, sigma))
+
+    @staticmethod
+    def energy_scale(E):
+        # 0.0018 / 0.9514 ~ 0.2%
+        return 0.002*E + 0.015
 
 
 def task_1():    
@@ -131,46 +196,31 @@ def task_2():
             else:
                 first_event = 0
                 
-            for event_number, event in enumerate(tree):
+            for event_number, root_event in enumerate(tree):
                 if event_number < first_event:
                     continue
 
-                #if event_number % 10 != 0:
-                #    continue
-                                        
-                #if hists["E_T_el"].count() > 1000:
-                #    break
-                    
+                if event_number % 100 != 0:
+                    continue
+                                   
                 if event_number % 1000 == 0:
                     print("\r", name, "- progress: %.1f %%           " % (100*float(event_number)/float(N_events)), end="")
                     sys.stdout.flush()
 
-                if hasattr(event, "weight"):
-                    weight = event.weight[9]
-                else:
-                    weight = 1.0
+                event = MyEvent(root_event)
                 
-                E_T_el = math.sqrt(event.el_e**2 - event.el_pz**2)
-                E_T_miss = math.sqrt(event.metx_calo**2 + event.mety_calo**2)
-                phi_miss = math.atan2(event.metx_calo, event.mety_calo) + math.pi
-                delta_z = event.el_track_z - event.met_vertex_z
-                m_T = math.sqrt(E_T_el*E_T_miss*(1-math.cos(event.el_met_calo_dphi)))
-
-                cuts = True
-                if cuts:
-                    # GOOD CUTS!
-                    if E_T_miss < 20 or E_T_el < 30 or event.el_iso > 0.03 \
-                        or event.el_met_calo_dphi < 2.85 or abs(delta_z) > 0.2:
-                        continue
+                if not event.passes_cuts():
+                    continue
                 
-                hists["E_T_el"].fill(E_T_el, weight)
-                hists["E_T_miss"].fill(E_T_miss, weight)
-                hists["m_T"].fill(m_T, weight)
+                weight = event.get_weight(9)
+                hists["E_T_el"].fill(event.el_e_t, weight)
+                hists["E_T_miss"].fill(event.met_e_t, weight)
+                hists["m_T"].fill(event.m_t, weight)
                 hists["eta_el"].fill(event.el_eta, weight)
                 hists["phi_el"].fill(event.el_phi, weight)
-                hists["phi_miss"].fill(phi_miss, weight)
+                hists["phi_miss"].fill(event.met_phi, weight)
                 hists["delta_phi"].fill(event.el_met_calo_dphi, weight)
-                hists["delta_z"].fill(delta_z, weight)
+                hists["delta_z"].fill(event.delta_z, weight)
 
         xlabels = {
             "E_T_el": "Electron Transverse Energy / GeV",
@@ -212,113 +262,183 @@ def task_2():
 def task_3():
     # CACHE
     mc_allcuts = 74072. 
-    #mc_allcuts = 187647. 
     data = 67329.
 
-    #mc_gen_count = 1164699.
     process_xsec = ufloat(2.58e3, 0.09e3)
-    #correction = ufloat(0.9, 0.1)
-    #luminosity = ufloat(198., 20.)
-
     n_data = ufloat(data, math.sqrt(data))
-
-    #lumi_scale = luminosity * process_xsec / mc_gen_count * correction
-    #mc_gen_count *= lumi_scale
-    
-    #efficiency = mc_allcuts / mc_gen_count
-    #xsec = n_data / (luminosity * efficiency * correction)
-
     xsec = n_data / mc_allcuts * process_xsec
 
-    #print("Efficiency: {eff:.1f} %%".format(eff=efficiency*100))
     print("XSec: {xsec:.2f} nb".format(xsec=xsec*1e-3))
 
 def task_4():
     with contextlib.nested(root_open("data/mc_all.root"), root_open("data/d0.root")) as (mc_file, data_file):
-        mc_hist = list( Hist(40, 70, 50) for _ in range(19) )
-        data_hist = Hist(40, 70, 50)
+        mc_hist_m_t = list( Hist(40, 70, 50) for _ in range(19) )
+        mc_hist_el_e_t = list( Hist(30, 70, 50) for _ in range(19) )
+        data_hist_m_t = Hist(40, 70, 50)
+        data_hist_el_e_t = Hist(30, 70, 50)
 
         for name, tree in zip(("mc", "data"), (mc_file.MCTree, data_file.MessTree)):
             N_events = tree.GetEntries()
-            for event_number, event in enumerate(tree):
-                #if event_number % 5 != 0:
+            for event_number, root_event in enumerate(tree):
+                #if event_number % 50 != 0:
                 #    continue
                     
                 if event_number % 1000 == 0:
                     print("\rProgress: %.1f %%" % (100*float(event_number)/float(N_events)), end="")
                     sys.stdout.flush()
-                    
-                E_T_el = math.sqrt(event.el_e**2 - event.el_pz**2)
-                E_T_miss = math.sqrt(event.metx_calo**2 + event.mety_calo**2)
-                m_T = math.sqrt(E_T_el*E_T_miss*(1-math.cos(event.el_met_calo_dphi)))
-                delta_z = event.el_track_z - event.met_vertex_z
+
+                event = MyEvent(root_event, scale_energy="down")
                 
-                if E_T_miss < 20 or E_T_el < 30 or event.el_iso > 0.03 \
-                    or event.el_met_calo_dphi < 2.85 or abs(delta_z) > 0.2:
+                if not event.passes_cuts():
                     continue
 
-                if name == "data":
-                    data_hist.fill(m_T)
-                elif name == "mc":
+                if event.is_mc():
                     for i, weight in enumerate(event.weight):
-                        mc_hist[i].fill(m_T, weight)
+                        mc_hist_m_t[i].fill(event.m_t, weight)
+                        mc_hist_el_e_t[i].fill(event.el_e_t, weight)
                 else:
-                    raise ValueError("WTF")
+                    data_hist_m_t.fill(event.m_t)
+                    data_hist_el_e_t.fill(event.el_e_t)
 
 
-    X = np.zeros(19)
-    Y = np.zeros(19)
-    for weight_index in range(19):
-
-        factor = data_hist.count() / mc_hist[weight_index].count()
-        mc_hist[weight_index].rescale(factor)
+    for name, mc_hist, data_hist in zip(("m_t", "el_e_t"), (mc_hist_m_t, mc_hist_el_e_t), (data_hist_m_t, data_hist_el_e_t)):
+        X = np.zeros(19)
+        Y = np.zeros(19)
         
-        Nmc = mc_hist[weight_index].histogram
-        Ndata = data_hist.histogram
+        plt.clf()
+        for weight_index in range(19):
+
+            factor = data_hist.count() / mc_hist[weight_index].count()
+            mc_hist[weight_index].rescale(factor)
+
+            assert(mc_hist[weight_index].min == data_hist.min)
+            assert(mc_hist[weight_index].max == data_hist.max)
+            assert(mc_hist[weight_index].nbins == data_hist.nbins)
+            
+            Nmc = mc_hist[weight_index].histogram
+            Ndata = data_hist.histogram
+            
+            error = np.sqrt(np.maximum(Ndata, 1))
+            chisquare = (np.power(Nmc-Ndata, 2)/np.power(error, 2)).sum() 
+            
+            X[weight_index] = weight_to_mass(weight_index)
+            Y[weight_index] = chisquare
+
+            if weight_index in (0, 18):
+                mc_hist[weight_index].steps(label="MC m=%.3f GeV" % weight_to_mass(weight_index))
+
+        data_hist.errorbar(label="Data", fmt=",")
+        #data_hist.annotate("$\\chi^2$ = %.1f" % chisquare, loc=2)
+
+        if name == "m_t":
+            plt.xlabel("Transverse Mass / GeV")
+        elif name == "el_e_t":
+            plt.xlabel("Electron Transverse Energy / GeV")
+        plt.ylabel("Number of Events")
+        plt.legend()
+        plt.savefig("comparison_%s.pdf" % name)
+        #plt.show()
+
+        print()
+
+        plt.clf()
+        plt.plot(X,Y, 's', color="black")
         
-        error = np.sqrt(np.maximum(Ndata, 1))
-        chisquare = (np.power(Nmc-Ndata, 2)/np.power(error, 2)).sum() 
+        try:
+            parabel = lambda x, w_mass, minimum, sigma: np.power((x-w_mass)/sigma, 2) + minimum
+            min_mass = weight_to_mass(0)
+            max_mass = weight_to_mass(18)
+            window = 0.3 # GeV
+            for i in range(2):
+                f = np.logical_and(X>=min_mass, X<=max_mass)
+                popt, pcov = curve_fit(parabel, X[f], Y[f], p0=((max_mass+min_mass)/2, data_hist.nbins, 0.05))
+                min_mass = max(popt[0] - window, weight_to_mass(0))
+                max_mass = min(popt[0] + window, weight_to_mass(18))
+                
+            w_mass, minimum, sigma = popt
+            sigma = abs(sigma)
+
+            xnew = np.linspace(X.min(), X.max(), 1000)
+            ynew = parabel(xnew, *popt)
+            
+            plt.axvline(w_mass, ls="-", color="black")
+            plt.axhline(minimum+1, ls="--", color="grey")
+            plt.axvline(w_mass-sigma, ls="--", color="grey")
+            plt.axvline(w_mass+sigma, ls="--", color="grey")
+            plt.plot(xnew,ynew, '-', color="grey")
+            print("W mass:", w_mass, "GeV")
+            u_w_mass = ufloat(w_mass, sigma)
+            annotate("Fitted W Mass: ${m:L}$ GeV".format(m=u_w_mass))
+            plt.ylim(minimum-1, minimum+10)
+            
+        except Exception as e:
+            print("Error:", e)
         
-        X[weight_index] = weight_to_mass(weight_index)
-        Y[weight_index] = chisquare
+        plt.xlabel("W Boson Mass / GeV")
+        plt.ylabel(r"$\chi^2$")
+        #plt.grid()
+        plt.savefig("chisquare_%s.pdf" % name)
+        #plt.show()
 
-        if weight_index in (0, 18):
-            #plt.clf()
-            mc_hist[weight_index].steps(label="MC")
-            data_hist.errorbar(label="Data", fmt=",")
-            data_hist.annotate("Chi^2 = %.1f" % chisquare, loc=2)
+def task_5():
+    with root_open("data/d0.root") as data_file:
+        X = []
+        Y = []
+        
+        N_events = data_file.MessTree.GetEntries()
+        for event_number, event in enumerate(data_file.MessTree):                
+            if event_number % 1000 == 0:
+                print("\rProgress: %.1f %%" % (100*float(event_number)/float(N_events)), end="")
+                sys.stdout.flush()
 
-    plt.xlabel("Transverse Mass / GeV")
-    plt.ylabel("Number of Events")
-    plt.legend()
-    plt.savefig("comparison.pdf")
-    plt.show()
+            event = MyEvent(event)
+            if not event.passes_cuts():
+                continue
 
-    print()
-       
-    parabel = lambda x, a, b, c: c*np.power(x-a, 2) + b
-    min_mass = weight_to_mass(0)
-    max_mass = weight_to_mass(18)
-    window = 0.2 # GeV
-    for i in range(5):
-        f = np.logical_and(X>=min_mass, X<=max_mass)
-        popt, pcov = curve_fit(parabel, X[f], Y[f], p0=(80, 100, 100))
-        min_mass = popt[0] - window
-        max_mass = popt[0] + window
+            X.append(event.m_t)
+            Y.append(event.el_e_t)
 
-    xnew = np.linspace(X.min(), X.max(), 1000)
-    ynew = parabel(xnew, *popt)
+        X = np.array(X)
+        Y = np.array(Y)
 
-    print("W mass:", popt[0], "GeV")
+        #correlation = (( X-X.mean() )*( Y-Y.mean() )).sum() / np.sqrt(np.power(X-X.mean(), 2).sum()*np.power(Y-Y.mean(), 2).sum())
+        correlation = np.corrcoef(X, Y)[0][1]
+        annotate("Correlation: %.3f" % correlation, loc=2)
+        plt.xlabel("Transverse Mass / GeV")
+        plt.ylabel("Electron Transverse Energy / GeV")
+        plt.plot(X[::10], Y[::10], ",", color="black")
+        plt.xlim(20,100)
+        plt.ylim(20,70)
+        plt.savefig("correlation.pdf")
+        plt.show()
+
+def task_6():
+    m_w_m_t = ufloat(80.21, 0.05)
+    m_w_el_e_t = ufloat(80.46, 0.06)
+
+    print("W Mass M_T:", m_w_m_t)
+    print("W Mass el_E_T:", m_w_el_e_t)
+
+    if m_w_m_t.s < m_w_el_e_t.s:
+        print("Using W Mass from M_T measurement.")
+        m_W = m_w_m_t
+    else:
+        print("Using W Mass from El_e_T measurement.")
+        m_W = m_w_el_e_t
+        
+    m_Z = ufloat(91.1876, 0.0021)
+
+    theta_W = umath.acos(m_W/m_Z)
+
+    print("W mass:", m_W, "GeV")
+    print("Z mass:", m_W, "GeV")
+    print("weinberg:", umath.degrees(theta_W), "degrees")
     
-    plt.clf()
-    plt.xlabel("W Boson Mass / GeV")
-    plt.ylabel("Chi^2")
-    plt.grid()
-    plt.plot(X,Y, 's', color="black")
-    plt.plot(xnew,ynew, '-', color="grey")
-    plt.savefig("chisquare.pdf")
-    plt.show()  
+    sin_theta_W_2 = 1 - umath.pow(m_W/m_Z, 2)
+    print("sin(weinberg)^2:", sin_theta_W_2)
+    
+    
                 
 if __name__=="__main__":
-    task_3()
+    for i in range(10000):
+        task_4()
